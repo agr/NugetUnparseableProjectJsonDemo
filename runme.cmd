5b046c15fe3365db670ba8a9e744af654854de34
Andrei Grigorev <angrigor@microsoft.com>
2017-05-19 11:50:45 -0700

    detailed verbosity

--- runme.cmd
+++ runme.cmd
@@ -1,2 +1,2 @@
-nuget.exe install -source http://api.dev.nugettest.org/v3-index/index.json
+nuget.exe install -source http://api.dev.nugettest.org/v3-index/index.json -verbosity detailed
 echo %ERRORLEVEL%
